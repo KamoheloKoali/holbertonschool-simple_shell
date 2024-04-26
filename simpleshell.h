f70037c380189b3e593eaeb2894c121342f6dee0
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-04-26 15:19:17 +0200

    add function prototypes

--- simpleshell.h
+++ simpleshell.h
@@ -12,6 +12,9 @@
 #include <sys/wait.h>
 
 int execute(char *const command[], char **envp);
+void input(char **command, size_t *size);
+void printerror(char *const command[]);
+char **pathfinder(char *cmd, char **command, char **envp);
 int print_env(void);
 char *_getenv(const char *name, char **envp);
 void parse(char command[], char **envp);
